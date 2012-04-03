2b41c143d5fc525b8a06e5df62acae8f925c5927
dgaletic <dinko.galetic@gmail.com>
2012-04-03 10:08:30 +0200

    A small script with the basic mechanism that will be used to stitch images.

---
+++ stitch_sample.py
@@ -0,0 +1,28 @@
+import SimpleCV
+import os
+import time
+from numpy import vstack,hstack
+
+
+
+c = SimpleCV.Camera()
+
+# Without this 0.5 sec sleep, the first image my camera takes is very dark, 
+# I suppose the issue is it not calibrating fast enough after turning on?
+# time.sleep(0.5)
+
+matrices = []
+
+for i in range(10):
+    img = c.getImage()
+    matrices.append(img.getNumpy())
+    time.sleep(0.5)
+
+mat = vstack(matrices)
+img_stitched = SimpleCV.Image(mat)
+print "Images saved in", os.getcwd()
+img_stitched.save("stitched_h.png")
+
+mat = hstack(matrices)
+img_stitched = SimpleCV.Image(mat)
+img_stitched.save("stitched_v.png")
